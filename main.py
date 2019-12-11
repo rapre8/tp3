189da2b18f89233d8236515c9e544c4e3069e715
Pierre-Luc Simard <pierre-luc.simard.10@ulaval.ca>
2019-12-10 20:31:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,25 @@ from quoridor import QuoridorError, Quoridor
 
 #première fonction, sert à récupérer les commanees tapées dans le terminal
 def analyser_commande():
-    parser = argparse.ArgumentParser(description="Jeu Quoridor")
+    parser = argparse.ArgumentParser(description="Jeu Quoridor - phase 3")
 
     parser.add_argument('idul', type=str, help='IDUL du joueur.')
 
-    parser.add_argument('-a', 'automatique', action = 'store_true',
-    help='pour jouer en mode automatique contre le serveur avec le nom spécifié')
+    parser.add_argument('-a', '--automatique', action = 'store_true',
+    help='Activer le mode automatique.')
 
-    parser.add_argument('-x', 'graphique', action = 'store_true',
-    help='pour jouer en mode manuel contre le serveur avec le nom spécifié, mais avec un affichage dans une fenêtre graphique.')
+    parser.add_argument('-x', '--graphique', action = 'store_true',
+    help='Activer le mode graphique.')
 
-    parser.add_argument('-ax', 'automatique_graphique', action = 'store_true',
-    help='pour jouer en mode automatique contre le serveur avec le nom spécifié, mais avec un affichage dans une fenêtre graphique.')
     args = parser.parse_args()
     analyser_commande.idul = args.idul
     
+    if args.automatique:
+        print('auto')
+    
+    if args.graphique:
+        print('graphe')
+
     return args
 
 #sert à activer la fonction analyser commande
