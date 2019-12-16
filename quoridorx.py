afd73c2808b44ac4b416edea6569743bbb13a68a
David <57725336+caf3ine-addik@users.noreply.github.com>
2019-12-16 16:13:39 -0500

    début formation des carrés

--- quoridorx.py
+++ quoridorx.py
@@ -1,7 +1,13 @@
 import turtle
 
-
 class QuoridorX(Quoridor):
         def __init__(self):
             super().__init__()
-        
+        def square(self, yoda):
+            yoda = turtle.Turtle()
+            yoda.color("black")
+            yoda.begin_fill()
+            for x in range(4):
+                yoda.forward(100)
+                yoda.right(90)
+            yoda.end_fill()
