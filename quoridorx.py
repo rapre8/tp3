b8b750feb3a5af93f0013c3ba474cc07f9ffd05b
rapre8 <rapre8@users.noreply.github.com>
2019-12-16 18:57:04 -0500

    Update quoridorx.py

--- quoridorx.py
+++ quoridorx.py
@@ -1,18 +1,19 @@
 import turtle
+from quoridor import Quoridor, QuoridorError
 
 
 class QuoridorX(Quoridor):
     def __init__(self):
         super().__init__()
     
-    def square(yoda, size):
+    def square(self, yoda, size):
         wn = turtle.Screen()
-        yoda = turtle.Turtle()
+        self.yoda = turtle.Turtle()
         wn.bgcolor("white")
         yoda.color("black")
         yoda.speed(0)
         yoda.begin_fill()
-        for i in range(4):
+        for _ in range(4):
             yoda.forward(size)
             yoda.right(90)
         yoda.end_fill()
@@ -20,17 +21,17 @@ class QuoridorX(Quoridor):
         yoda.forward(size + 40)
         yoda.pendown()
 
-    def ligne_damier(yoda, size):
-        for i in range(10):
-            square(yoda, size)
+    def ligne_damier(self, yoda, size):
+        for _ in range(10):
+            self.square(yoda, size)
     
-    def damier_complet(yoda, size, n):
+    def damier_complet(self, yoda, size, n):
         yoda.goto(4*(-size + 40), (size+40))
 
-        for i in range(n):
-            ligne_damier(yoda, size)
+        for _ in range(n):
+            self.ligne_damier(yoda, size)
             yoda.backward(9*(size+40))
             yoda.right(90)
 
     def afficher_damier(self):
-        damier_complet(yoda, 100)
+        self.damier_complet(self.yoda, 100, 10)
